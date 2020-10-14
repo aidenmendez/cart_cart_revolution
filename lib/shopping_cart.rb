58b7bafc4f8ba9991a86a406b339de965804e13c
Aiden Mendez <aidenmendez@gmail.com>
2020-10-14 14:00:34 -0600

    Complete iteration 3

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -22,4 +22,22 @@ class ShoppingCart
     }
   end
 
+  def total_number_of_products
+    quantity_total = 0
+    products.each do |product|
+      quantity_total += product.quantity
+    end
+    quantity_total
+  end
+
+  def is_full?
+   total_number_of_products > capacity 
+  end
+
+  def products_by_category(category)
+    filtered_products = products.find_all do |product|
+      product.category == category
+    end
+  end
+
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -45,4 +45,56 @@ class ShoppingCartTest < Minitest::Test
     assert_instance_of Hash, cart.details
     assert_equal 2, cart.details.count
   end
+
+  def test_total_number_of_items_in_cart
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart = ShoppingCart.new("King Soopers", "30items")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal 13, cart.total_number_of_products
+  end
+
+  def test_cart_is_not_full
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart = ShoppingCart.new("King Soopers", "30items")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal false, cart.is_full?
+  end
+
+  def test_cart_is_full
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart = ShoppingCart.new("King Soopers", "30items")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert cart.is_full?
+  end
+
+  def test_filter_products_by_category
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart = ShoppingCart.new("King Soopers", "30items")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert_equal [product1, product3], cart.products_by_category(:paper)
+  end
 end
